120f56a48f52f97a32627e1ea7b800e5f6587753
clear <you@example.com>
2022-01-24 00:03:00 -0500

    commits

---
+++ main.js
@@ -0,0 +1,28 @@
+var events = require("events");
+
+//create your event emitter
+var eventEmitter = new events.EventEmitter();
+
+//create an event handler, a simply function that indicates a user just logged-in
+var authHandler = () => {
+  console.log("User just logged-in.");
+  //fire the event login success
+  eventEmitter.emit("login_ok");
+};
+
+//bind the login event with the handler
+eventEmitter.on("login", authHandler);
+//bind the login_ok event with some other anonymous handler
+eventEmitter.on("login_ok", () => {
+  console.log("Login success confirmed!");
+});
+
+//fire the events
+eventEmitter.emit("login");
+
+
+// const greet = (name)=>{
+//   console.log(`Hello ${name}`)
+// }
+
+// greet(`John`)
